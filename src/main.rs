4f509acdd1704be6ff5ff11d81a0fc6f37050e36
gaprop <anders.kildemand@gmail.com>
2023-01-04 07:16:47 +0100

    First commit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use csv::{ReaderBuilder, Reader, StringRecord};
+use std::error::Error;
+use std::io;
+use std::io::Stdin;
+use std::process;
+#[macro_use]
+extern crate prettytable;
+
+use prettytable::Table;
+
+fn read_data() -> Reader<Stdin> {
+    ReaderBuilder::new()
+        .has_headers(false)
+        .from_reader(io::stdin())
+}
+
+
+fn main() {
+    let mut data = read_data();
+    let table = Table::from_csv(&mut data);
+    table.printstd();
+}
