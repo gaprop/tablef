e4dabf9fa32615572070a70d4bbd719513e6ce2d
gaprop <anders.kildemand@gmail.com>
2023-01-04 17:56:34 +0100

    Added better documentation in the -h output

--- src/main.rs
+++ src/main.rs
@@ -6,34 +6,71 @@ use std::io::Stdin;
 
 extern crate prettytable;
 
-/// A program to pretty print tables
+/// A program to pretty print tables.
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
+#[command(next_line_help = true)]
 struct Cli {
-    /// Set the style spec for every cell
-    #[arg(short, long, value_name = "SPEC")]
+    /// Set the style spec for every cell.
+    /// The syntax for the style spec looks like this : FrBybl which means Foreground red Background yellow bold left.
+    /// List of supported specifiers :
+    /// F : Foreground (must be followed by a color specifier)
+    /// B : Background (must be followed by a color specifier)
+    /// H : Horizontal span (must be followed by a number)
+    /// b : bold
+    /// i : italic
+    /// u : underline
+    /// c : Align center
+    /// l : Align left
+    /// r : Align right
+    /// d : default style
+    /// List of color specifiers :
+    /// r : Red
+    /// b : Blue
+    /// g : Green
+    /// y : Yellow
+    /// c : Cyan
+    /// m : Magenta
+    /// w : White
+    /// d : Black
+    /// And capital letters are for bright colors. Eg :
+    /// R : Bright Red
+    /// B : Bright Blue
+    /// … and so on …
+    #[arg(short, long, value_name = "SPEC", verbatim_doc_comment)]
     all: Option<String>,
 
-    /// Set the style for a specific column
-    #[arg(short, long, value_name = "SPEC", action = clap::ArgAction::Append)]
+    /// Set the style for a specific column, where INDEX is the index of the clumn
+    #[arg(short, long, value_name = "INDEX,SPEC", action = clap::ArgAction::Append)]
     column: Option<Vec<String>>,
 
-    /// Set the style spec for a specific row
-    #[arg(short, long, value_name = "SPEC", action = clap::ArgAction::Append)]
+    /// Set the style spec for a specific row, where INDEX is the index of the row
+    #[arg(short, long, value_name = "INDEX,SPEC", action = clap::ArgAction::Append)]
     row: Option<Vec<String>>,
+
+    /// Set the delimiter
+    #[arg(short, long, default_value_t = ',')]
+    delimiter: char
 }
 
 
-fn read_data() -> Reader<Stdin> {
+fn read_data(delimiter: u8) -> Reader<Stdin> {
     ReaderBuilder::new()
         .has_headers(false)
+        .delimiter(delimiter)
         .from_reader(io::stdin())
 }
 
 fn main() {
     let cli = Cli::parse();
-    let mut data = read_data();
+    let delimiter: u8 = cli.delimiter
+                       .to_string()
+                       .as_bytes()[0];
+    let mut data = read_data(delimiter);
     let mut table = Table::from_csv(&mut data);
+    // let delimiter = vec![cli.delimiter];
+    // let delimiter: &str = std::str::from_utf8(&delimiter)
+        // .expect("None acceptable delimiter");
 
     if let Some(all) = cli.all.as_deref() {
         for row in table.row_iter_mut() {
@@ -45,7 +82,7 @@ fn main() {
 
     if let Some(columns) = cli.column.as_deref() {
         for column in columns {
-            let mut column = column.split(",");
+            let mut column = column.split(cli.delimiter);
             let id = column
                      .next()
                      .expect("The first value has to be the number of the column that has to be changed")
@@ -60,7 +97,7 @@ fn main() {
 
     if let Some(rows) = cli.row.as_deref() {
         for row in rows {
-            let mut row = row.split(",");
+            let mut row = row.split(cli.delimiter);
             let id = row
                      .next()
                      .expect("The first value has to be the number of the row that has to be changed")
